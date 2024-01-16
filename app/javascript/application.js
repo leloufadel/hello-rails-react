1b5960bfd7d9161758b18f8d39547dafcfc2fd05
leloufadel <leloufadel@gmail.com>
2024-01-16 11:13:40 +0000

    update app.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import App from './components/App';
+// import '../assets/stylesheets/main.css';
 
 ReactDOM.render(
-  <App/>,
+  <App />,
   document.getElementById('root'),
 );
